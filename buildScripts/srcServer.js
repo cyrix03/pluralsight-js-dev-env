dd214b398af3aad46de3b7606653417726589e9a
cyrix03 <a89.warren@gmail.com>
2017-05-04 02:23:42 -0500

    added new features 5_4_17

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -4,13 +4,23 @@
 //var open = require('open');
 //var port = 3000;
 //var app = express();
-
+/* eslint-disable no-console */
 import express from 'express';
 import path from 'path';
 import open from 'open';
+import webpack from 'webpack';
+import config from '../webpack.config.dev';
+
+
 
 const port = 3000;
 const app = express();
+const compiler = webpack(config);
+
+app.use(require('webpack-dev-middleware')(compiler,{
+  noInfo: true,
+  publicPath: config.output.publicPath
+}));
 
 app.get('/', function(req,res){
   res.sendFile(path.join(__dirname,'../src/index.html'));
