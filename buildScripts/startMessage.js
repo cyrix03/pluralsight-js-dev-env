e47a222277abc700e71224423c500869db7be40a
andrew warren <a89.warren@gmail.com>
2017-05-04 23:34:32 -0500

    added base testing eliments

--- buildScripts/startMessage.js
+++ buildScripts/startMessage.js
@@ -1,5 +1,5 @@
 //rewrote to use ES6 old way below
 //var chalk = require('chalk');
-import chalk from 'chalk';
+import chalk from 'Chalk';
 
 console.log(chalk.green('Starting app in dev mode...')); // eslint-disable-line no-console
